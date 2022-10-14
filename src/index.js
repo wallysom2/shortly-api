30e8d0d961a60dd00b013c3f13ed753cacb663a6
Wallyson <wallysom2@gmail.com>
2022-10-14 10:40:26 -0300

    feat: criando rotas de cadastro

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import express, {json} from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 
 const app = express();
 
 dotenv.config();
 app.use (json());
+app.use (cors());
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
