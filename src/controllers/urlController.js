f1b627cbdf4be198d6bd13d5d1e73c8ee264527c
Wallyson <wallysom2@gmail.com>
2022-10-17 15:49:19 -0300

    feat: configurando controller que faz o encurtamento da url

--- src/controllers/urlController.js
+++ src/controllers/urlController.js
@@ -18,10 +18,58 @@ export async function shortenUrl (req, res) {
 }
 
 export async function getURLById (req, res) {
+    const {id} = req.params;
+    try {
+     const urlresult = await urlRepo.getURLById(id);
+     if (urlresult.rowCount === 0) {
+         return res.sendStatus(404);
+     }
+        const [url] = urlresult.rows;
+        delete url.user_id;
+        delete url.contador;
+
+        res.send (url);
+    }
+    catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
 }
 
 export async function deleteURL (req, res) {
+    const {id} =  req.params;
+    const {user} = res.locals;
+    try {
+        const urlResult = await urlRepo.getURLById(id);
+        if (urlResult.rowCount === 0) {
+            return res.sendStatus(404);
+        }
+        const [url] = urlResult.rows;
+        if (url.user_id !== user.id) {
+            return res.sendStatus(401);
+        }
+        await urlRepo.deleteURL(id);
+        res.sendStatus(204);
+    }
+    catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
 }
 
 export async function openShortUrl (req, res) {
+    const {shortUrl} = req.params;
+    try {
+        const urlResult = await urlRepo.getByShortURL(shortUrl);
+        if (urlResult.rowCount === 0) {
+            return res.sendStatus(404);
+        }
+        const [url] = urlResult.rows;
+        await urlRepo.incrementCounter(url.id);
+        res.redirect(url.url);
+    }
+    catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
 }
--- src/repositories/urlRepository.js
+++ src/repositories/urlRepository.js
@@ -4,8 +4,29 @@ async function createShortURL (url, shortUrl, userId) {
 return db.query("INSERT INTO urls (url, short_url, user_id) VALUES ($1, $2, $3) RETURNING *", [url, shortUrl, userId]);
 }
 
+async function getURLById (id) {
+    return db.query("SELECT * FROM urls WHERE id = $1", [id]);
+}
+
+async function deleteURL (id) {
+    return db.query("DELETE FROM urls WHERE id = $1", [id]);
+}
+
+async function getByShortURL (shortUrl) {
+    return db.query("SELECT * FROM urls WHERE short_url = $1", [shortUrl]);
+}
+
+async function incrementCounter (id) {
+    return db.query("UPDATE urls SET contador = contador + 1 WHERE id = $1", [id]);
+}
+
+
 const urlRepo = {
-createShortURL
+createShortURL,
+getURLById,
+deleteURL,
+getByShortURL,
+incrementCounter
 }
 
 export default urlRepo;
