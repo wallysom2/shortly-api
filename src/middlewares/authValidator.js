3cf983cd921316a8cd563e937e27784ed9b419c8
Wallyson <wallysom2@gmail.com>
2022-10-17 17:28:43 -0300

    feat: alteracao no validador do middleware

--- src/middlewares/authValidator.js
+++ src/middlewares/authValidator.js
@@ -5,26 +5,26 @@ export async function validateToken(req, res, next) {
   const authorization = req.headers.authorization;
   const token = authorization?.replace("Bearer ", "");
   if (!token) {
-    return res.send(401).status("No token."); // unauthorized
+    return res.send(401).status("No token.");
   }
 
   try {
     const { rows:sessions } = await sessionRepository.getSessionByToken(token);
     const [session] = sessions;
     if (!session) {
-      return res.send(401).send("Session not found."); // unauthorized
+      return res.send(401).send("Session not found.");
     }
 
     const { rows: users } = await usersRepository.getUserById(session.userId);
     const [user] = users;
     if (!user) {
-      return res.send(401).send("User not found."); // unauthorized
+      return res.send(401).send("User not found."); 
     }
   
     res.locals.user = user;
     next();
   } catch (error) {
     console.log(error);
-    return res.sendStatus(500); // server error
+    return res.sendStatus(500); 
   }  
 }
